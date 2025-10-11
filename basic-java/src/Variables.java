1f5558907230708604a39a7b3030b36ccce9ab6c
Alex Junior <alexjunior2018@gmail.com>
2025-10-11 00:58:41 -0300

    adicionando primeiro exemploa aula

---
+++ basic-java/src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        int xyz = 20;
+
+        int abc = 2;
+        int y = abc + 1;
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        double d1 = 10.2;
+        double d2 = 20.5;
+    }
+}
