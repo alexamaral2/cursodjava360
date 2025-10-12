98ea97c60f8a819d80991f69da2b9d2614e1c7d2
Alex Junior <alexjunior2018@gmail.com>
2025-10-11 23:16:06 -0300

    adicionando exercicios

---
+++ basic-java/src/NumberSplit.java
@@ -0,0 +1,13 @@
+public class NumberSplit {
+    public static void main(String[] args) {
+        int n = 476_879_386;
+        System.out.println(n);
+
+        double d = 4_387_489.4_3_4_3_5_6_7_9_8_2;
+        System.out.println(d);
+
+        //int x1 = _10;
+        //int x2 = 10_;
+        //double x3 = 10_.3;
+    }
+}
---
+++ basic-java/src/VarType.java
@@ -0,0 +1,9 @@
+public class VarType {
+    public static void main(String[] args) {
+
+        var a = 10;
+        //a = 12.0;
+
+        var b = 10L;
+    }
+}
