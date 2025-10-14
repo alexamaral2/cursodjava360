69f0ac22056b969add38d8100aad7580ac260c0f
Alex Junior <alexjunior2018@gmail.com>
2025-10-14 18:11:17 -0300

    adicionando exercicios

---
+++ basic-java/src/ImplicitCasting.java
@@ -0,0 +1,15 @@
+public class ImplicitCasting {
+    public static void main(String[] args) {
+
+        int i = 200;
+        double d = i;
+
+        System.out.println(i);
+        System.out.println(d);
+
+        double d2 = 200;
+        d2 = 200L;
+
+        long l = 500L;
+    }
+}
