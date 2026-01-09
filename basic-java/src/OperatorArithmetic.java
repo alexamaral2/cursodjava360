71627c9c53f3f046b767d5abdd061085f3115750
Alex Junior <alexjunior2018@gmail.com>
2026-01-09 00:52:09 -0300

    add exercises

---
+++ basic-java/src/OperatorArithmetic.java
@@ -0,0 +1,15 @@
+public class OperatorArithmetic {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a + 5;
+        int c = 5;
+        int d =  a * b - c;
+        System.out.println(b);
+
+        int e = 4;
+        int f = 2;
+        int g = e * f / e;
+        System.out.println(g);
+    }
+}
---
+++ basic-java/src/OperatorArithmeticDivision.java
@@ -0,0 +1,10 @@
+public class OperatorArithmeticDivision {
+    public static void main(String[] args) {
+
+        double a = 4;
+        int b = 2;
+        double c = a / b;
+        System.out.println(c);
+
+    }
+}
