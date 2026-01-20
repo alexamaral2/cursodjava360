98d88c911249b05eafb7360a8bb30634b1d68cd5
Alex Junior <alexjunior2018@gmail.com>
2026-01-20 17:35:41 -0300

    adicionando exercicios java

--- basic-java/src/Switch.java
+++ basic-java/src/Switch.java
@@ -1,23 +1,33 @@
 public class Switch {
     public static void main(String[] args) {
-        int month = 13;
-        int days = 0;
+        int month = 5;
+        int days;
 
         switch (month) {
             case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
                 days = 31;
                 break;
             case 2:
                 days = 28;
                 break;
-            case 3:
-                days = 31;
-                break;
             case 4:
+            case 6:
+            case 9:
+            case 11:
                 days = 30;
+                break;
+            default:
+                days = 0;
+                System.out.println("invalid month");
                 break;
         }
 
-        System.out.println(days + " dias");
+        System.out.println(days + " days");
     }
 }
