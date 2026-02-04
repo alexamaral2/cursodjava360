dbc2d0863c1316d324c01efa9d63dd7898d9b521
Alex Junior <alexjunior2018@gmail.com>
2026-02-04 00:43:14 -0300

    adicionando poo

---
+++ java-oo-1/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        Account a1 = new Account();
+        a1.accountNumber = "1234-5";
+
+        System.out.println(a1.accountNumber);
+    }
+}
