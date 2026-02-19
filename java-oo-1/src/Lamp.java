eff270c9e2de80ffe8ba939e26aae07a03d0013f
Alex Junior <alexjunior2018@gmail.com>
2026-02-18 23:33:38 -0300

    adicionando exercicio lampada

---
+++ java-oo-1/src/Lamp.java
@@ -0,0 +1,19 @@
+public class Lamp {
+    boolean on;
+
+    public Lamp(boolean on) {
+        this.on = on;
+    }
+
+    public void turnOn() {
+        on = true;
+    }
+
+    public void turnOff() {
+        on = false;
+    }
+
+    public void showState(){
+        System.out.println("Lâmpada " + (on ? "ligada" : "desligada"));
+    }
+}
