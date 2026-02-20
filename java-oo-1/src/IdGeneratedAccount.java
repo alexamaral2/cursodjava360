f26bcb87b41480405f5961a3ced1d0b680c1b2e6
Alex Junior <alexjunior2018@gmail.com>
2026-02-19 23:34:07 -0300

    adicionando exercicio

---
+++ java-oo-1/src/IdGeneratedAccount.java
@@ -0,0 +1,55 @@
+public class IdGeneratedAccount {
+    private final String accountNumber;
+    private final String accountOwner;
+    private double balance;
+
+    private static int currentId = 1;
+
+    public IdGeneratedAccount(String accountOwner) {
+        this.accountNumber = "000" + currentId++;
+        this.accountOwner = accountOwner;
+        this.balance = 0.0;
+    }
+
+    boolean deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            return true;
+        }
+        return false;
+    }
+
+    boolean deposit(String amount){
+        return deposit(Double.parseDouble(amount));
+    }
+
+    boolean withdraw(double amount) {
+        if (amount > 0) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    void transfer(double amount, EncapsulatedAccount targetAccount) {
+        withdraw(amount);
+        targetAccount.deposit(amount);
+    }
+
+    void printBalance(){
+        System.out.println("Balance: R$ " + balance);
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountOwner() {
+        return accountOwner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+}
