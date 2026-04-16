de27a29ed8cab24ced42a9a1cdbcc22e82aaaeab
Alex Junior <alexjunior2018@gmail.com>
2026-04-16 01:38:32 -0300

    adicionando exercicio de exception

---
+++ java-exceptions/src/account/Account3.java
@@ -0,0 +1,35 @@
+package account;
+
+public class Account3 {
+
+    private final String number;
+    private double balance;
+
+    public Account3(String number) {
+        this.number = number;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(double amount) throws Exception {
+        if (amount < 0){
+            throw new Exception("Amount cannot be negative");
+        }
+
+        if (this.balance - amount < 0) {
+            throw new Exception("Insufficient funds");
+        }
+
+        this.balance -= amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Account3{" +
+                "number='" + number + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
