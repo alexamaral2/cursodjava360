f328341c065052ca0ace72357a3fd35f3d8b0583
Alex Junior <alexjunior2018@gmail.com>
2026-06-14 00:57:41 -0300

    adicionando exercicios

---
+++ java-special-types/src/app/StringCompare.java
@@ -0,0 +1,12 @@
+package app;
+
+public class StringCompare {
+    public static void main(String[] args) {
+
+        String s1 = "bad";
+        String s2 = "chair";
+
+        int r = s1.compareTo(s2);
+        System.out.println(r);
+    }
+}
